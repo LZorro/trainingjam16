31438cccfab0f41d7a652e535f4961eae6492ab2
Tim Volpe <timothyvolpe@hotmail.com>
2016-04-23 11:12:30 -0700

    created mockup scene, started on XML file design

---
+++ SteamedCustomer/Assets/Scripts/CustomerDialog.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Xml;
+
+public class CustomerDialog : MonoBehaviour {
+
+	public TextAsset scriptFile;
+
+	XmlDocument dialogScript;
+	XmlNodeList scenarioList;
+
+	public Text customerPrompt;
+	public Button csrResponse1;
+
+	// Use this for initialization
+	void Start () {
+		InitScript();
+		LoadScenario();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void InitScript()
+	{
+		dialogScript = new XmlDocument();
+		dialogScript.LoadXml(scriptFile.text);
+		scenarioList = dialogScript.GetElementsByTagName("scenario");
+	}
+
+	void LoadScenario()
+	{
+		
+	}
+}
