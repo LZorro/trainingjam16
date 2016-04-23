0d5e219695f996b68c86bdee653e3deb940e3722
Tim Volpe <timothyvolpe@hotmail.com>
2016-04-23 13:07:48 -0700

    dialog branching in, starting face changes

---
+++ SteamedCustomer/Assets/Scripts/CSRResponseClick.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CSRResponseClick : MonoBehaviour {
+
+	public int emoDiff;
+	public int nextPrompt;
+	public GameObject customer;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void buttonClicked()
+	{
+		customer.GetComponent<CustomerDialog>().changeEmo(emoDiff, nextPrompt);
+	}
+}
--- SteamedCustomer/Assets/Scripts/CustomerDialog.cs
+++ SteamedCustomer/Assets/Scripts/CustomerDialog.cs
@@ -7,19 +7,22 @@ public class CustomerDialog : MonoBehaviour {
 
 	public TextAsset scriptFile;
 	public Text customerPrompt;
-	public Text csrResponse1;
-	public Text csrResponse2;
-	public Text csrResponse3;
-	public Text csrResponse4;
+	public Button csrResponse1;
+	public Button csrResponse2;
+	public Button csrResponse3;
+	public Button csrResponse4;
 
 	XmlDocument dialogScript;
 	XmlNodeList scenarioList;
 	int emoLevel;
+	int currentScenario;
 
 	// Use this for initialization
 	void Start () {
+		currentScenario = 0;
+
 		InitScript();
-		LoadScenario();
+		LoadScenario(currentScenario);
 	}
 	
 	// Update is called once per frame
@@ -34,43 +37,54 @@ public class CustomerDialog : MonoBehaviour {
 		scenarioList = dialogScript.GetElementsByTagName("scenario");
 	}
 
-	void LoadScenario()
+	void LoadScenario(int currSen)
 	{
-		XmlNode node1 = scenarioList[0];
+		XmlNode node1 = scenarioList[currSen];
 
 		foreach (XmlNode items in node1.ChildNodes)
 		{
 			if (items.Name == "prompt")
 			{
 				customerPrompt.text = items.InnerText;
-				Debug.Log("Prompt: " + items.InnerText);
 			}
 			else if (items.Name == "emoLevel")
 			{
 				emoLevel = int.Parse(items.InnerText);
-				Debug.Log("Emo Level: " + emoLevel);
+				Debug.Log("Start emo: " + emoLevel);
 			}
 			else if (items.Name == "response1")
 			{
-				csrResponse1.text = items.InnerText;
-				Debug.Log("Response: " + items.InnerText);
+				csrResponse1.GetComponentInChildren<Text>().text = items.InnerText;
+				csrResponse1.GetComponent<CSRResponseClick>().emoDiff = int.Parse(items.Attributes["diff"].Value);
+				csrResponse1.GetComponent<CSRResponseClick>().nextPrompt = int.Parse(items.Attributes["next"].Value);
 			}
 			else if (items.Name == "response2")
 			{
-				csrResponse2.text = items.InnerText;
-				Debug.Log("Response: " + items.InnerText);
+				csrResponse2.GetComponentInChildren<Text>().text = items.InnerText;
+				csrResponse2.GetComponent<CSRResponseClick>().emoDiff = int.Parse(items.Attributes["diff"].Value);
+				csrResponse2.GetComponent<CSRResponseClick>().nextPrompt = int.Parse(items.Attributes["next"].Value);
 			}
 			else if (items.Name == "response3")
 			{
-				csrResponse3.text = items.InnerText;
-				Debug.Log("Response: " + items.InnerText);
+				csrResponse3.GetComponentInChildren<Text>().text = items.InnerText;
+				csrResponse3.GetComponent<CSRResponseClick>().emoDiff = int.Parse(items.Attributes["diff"].Value);
+				csrResponse3.GetComponent<CSRResponseClick>().nextPrompt = int.Parse(items.Attributes["next"].Value);
 			}
 			else if (items.Name == "response4")
 			{
-				csrResponse4.text = items.InnerText;
-				Debug.Log("Response: " + items.InnerText);
+				csrResponse4.GetComponentInChildren<Text>().text = items.InnerText;
+				csrResponse4.GetComponent<CSRResponseClick>().emoDiff = int.Parse(items.Attributes["diff"].Value);
+				csrResponse4.GetComponent<CSRResponseClick>().nextPrompt = int.Parse(items.Attributes["next"].Value);
 			}
 		}
 			
+	}
+
+	public void changeEmo(int diff, int nextprompt)
+	{
+		emoLevel += diff;
+		Debug.Log("New emo: " + emoLevel);
+
+		LoadScenario(nextprompt);
 	}
 }
